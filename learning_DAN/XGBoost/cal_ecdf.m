5da24dccb3b1a63fd38ed08146ec1cec80c6a1de
HaTT2018 <zkaihang17@gmail.com>
2021-09-25 18:03:09 -0700

    update XFBoost

---
+++ learning_DAN/XGBoost/cal_ecdf.m
@@ -0,0 +1,32 @@
+clear all
+y_pre=[];
+y_true=[];
+for i=5:5
+    eval( ['load',' result',num2str(i),'.mat'] );
+    y_pre=[y_pre;pre];
+    y_true=[y_true;true];
+end
+pre=[];true=[];
+y_pre=y_pre';y_true=y_true';
+for i=1:25
+        pre(i,:)=reshape(y_pre(i*1008-1007:i*1008,:),[1,1008]);
+        true(i,:)=reshape(y_true(i*1008-1007:i*1008,:),[1,1008]);
+end
+sub=pre-true;
+add=pre+true;
+total=[];
+total(1)=sqrt(sum(sum(sub.^2))/sum(sum(true.^2)));
+total(2)=sqrt(mean(mean(sub.^2)));
+total(3)=mean(mean(abs(sub)./add));
+for i=1:25
+    pre_temp=pre(i,:);
+    true_temp=true(i,:);
+    sub_temp=pre_temp-true_temp;
+    sum_temp=pre_temp+true_temp;
+    nrmse(i)=sqrt(sum(sum(sub_temp.^2))/sum(sum(true_temp.^2)));
+    rmse(i)=sqrt(mean(mean(sub_temp.^2)));
+    smape(i)=mean(mean(abs(sub_temp)./sum_temp));
+    mape(i)=mean(abs(sub_temp)./abs(true_temp));
+end
+disp(mean(mape))
+save(char(['C:\Users\10169\Documents\Github\NET_louvain_DAN\model\base_XGBoost_mape=', num2str(mean(mape)),'.mat']))
---
+++ learning_DAN/XGBoost/dataprocess.m
@@ -0,0 +1,25 @@
+clear all
+load ../RF/V.mat
+V2=V2';
+pre=[];
+x_train=[];
+y_train=[];
+x_test=[];
+y_test=[];
+for i=1:25
+    temp1=[];
+    temp2=[];
+    temp3=[];
+    temp4=[];
+    for j=1:144*7
+      temp1=[temp1;V2(i,j:j+11)];
+      temp2=[temp2;V2(i,j+17)];
+      temp3=[temp3;V2(i,j+144*7:j+144*7+11)];
+      temp4=[temp4;V2(i,j+144*7+17)];
+    end
+    x_train=[x_train;temp1];
+    y_train=[y_train;temp2];
+    x_test=[x_test;temp3];
+    y_test=[y_test;temp4];
+end
+save('data2_6.mat','x_train','y_train','x_test','y_test');
