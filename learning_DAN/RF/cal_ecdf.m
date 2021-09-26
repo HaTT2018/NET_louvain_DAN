debd0d15897e34b247ef4c9dbce44d92d56713c8
HaTT2018 <zkaihang17@gmail.com>
2021-09-25 18:02:55 -0700

    update RF

--- learning_DAN/RF/cal_ecdf.m
+++ learning_DAN/RF/cal_ecdf.m
@@ -8,7 +8,7 @@ for i=1:1
 end
 pre=[];true=[];
 y_pre=y_pre';y_true=y_true';
-for i=1:308
+for i=1:25
         pre(i,:)=reshape(y_pre(i*1008-1007:i*1008,:),[1,1008]);
         true(i,:)=reshape(y_true(i*1008-1007:i*1008,:),[1,1008]);
 end
@@ -18,7 +18,7 @@ total=[];
 total(1)=sqrt(sum(sum(sub.^2))/sum(sum(true.^2)));
 total(2)=sqrt(mean(mean(sub.^2)));
 total(3)=mean(mean(abs(sub)./add));
-for i=1:308
+for i=1:25
     pre_temp=pre(i,:);
     true_temp=true(i,:);
     sub_temp=pre_temp-true_temp;
@@ -26,4 +26,7 @@ for i=1:308
     nrmse(i)=sqrt(sum(sum(sub_temp.^2))/sum(sum(true_temp.^2)));
     rmse(i)=sqrt(mean(mean(sub_temp.^2)));
     smape(i)=mean(mean(abs(sub_temp)./sum_temp));
+    mape(i)=mean(abs(sub_temp)./abs(true_temp));
 end
+disp(mean(mape))
+save(char(['C:\Users\10169\Documents\Github\NET_louvain_DAN\model\base_RF_mape=', num2str(mean(mape)),'.mat']))
